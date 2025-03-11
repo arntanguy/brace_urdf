48da31d567c860455b01b41acb6745dbf797995b
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-11 10:23:29 +0100

    Setup and run pre-commit

--- cmake/brace_urdf_INSTALL_PREFIX.cmake
+++ cmake/brace_urdf_INSTALL_PREFIX.cmake
@@ -1,2 +1,4 @@
 set(brace_urdf_INSTALL_PREFIX "${brace_urdf_DIR}/../")
-get_filename_component(brace_urdf_INSTALL_PREFIX "${brace_urdf_INSTALL_PREFIX}" REALPATH)
+get_filename_component(
+  brace_urdf_INSTALL_PREFIX "${brace_urdf_INSTALL_PREFIX}" REALPATH
+)
