53ed6540e8a8957dfe90a8dc6467bdbf5a35f522
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-11 10:29:14 +0100

    improve cmake export

--- cmake/brace_urdf_INSTALL_PREFIX.cmake
+++ cmake/brace_urdf_INSTALL_PREFIX.cmake
@@ -1,4 +1,4 @@
-set(brace_urdf_INSTALL_PREFIX "${brace_urdf_DIR}/../")
+cmake_path(SET brace_urdf_INSTALL_PREFIX NORMALIZE "${brace_urdf_DIR}/../")
 get_filename_component(
   brace_urdf_INSTALL_PREFIX "${brace_urdf_INSTALL_PREFIX}" REALPATH
 )
